2d16b66048ea8ba8ac9279b38a32008b530df0f6
EC2 Default User <ec2-user@ip-172-31-11-233.us-east-2.compute.internal>
2018-04-10 08:45:57 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
     @items = []
 
     @keyword = params[:keyword]
-    if @keyword.present? # 
+    if @keyword
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
@@ -13,9 +13,14 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
+    end
+    
+    def show
+      @item = Item.find(params[:id])
+      @want_users = @item.want_users
     end
   end
 
