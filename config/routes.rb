e4423fc8e2f6f48309303cf2dca5e718f27b1042
EC2 Default User <ec2-user@ip-172-31-11-233.us-east-2.compute.internal>
2018-04-10 05:31:07 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root to: 'toppages#index'
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+
+  resources :items, only: [:new]
 end
